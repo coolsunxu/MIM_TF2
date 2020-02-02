7934eed1587a91bb3468d3ac44b1e66cbec94bc9
coolsunxu <39801564+coolsunxu@users.noreply.github.com>
2020-02-02 21:04:29 +0800

    Update TensorLayerNorm.py

--- layers/TensorLayerNorm.py
+++ layers/TensorLayerNorm.py
@@ -1,8 +1,9 @@
+
 import tensorflow as tf
+from tensorflow.keras import layers # 导入常见网络层类
 
+"""
 EPSILON = 0.00001
-
-
 def tensor_layer_norm(x, state_name):
 	x_shape = x.get_shape() # 获得输入的大小
 	dims = x_shape.ndims
@@ -21,3 +22,10 @@ def tensor_layer_norm(x, state_name):
 	s = tf.Variable(initial_value=tf.ones(params_shape),name = state_name+'s',trainable=False)
 	x_tln = tf.nn.batch_normalization(x, m, v, b, s, EPSILON) # 归一化
 	return x_tln
+"""
+
+# 返回归一化层
+def tensor_layer_norm(state_name):
+	return layers.BatchNormalization(name = state_name)
+
+
